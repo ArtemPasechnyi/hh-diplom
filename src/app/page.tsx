5158fb65907b9912d4695ee4b237c2ce865d16be
coca8732 <nub465@gmail.com>
2024-03-29 14:29:48 +0300

    стили глобальные

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main>
-      <div className="m-8 flex gap-4">
-        <div>Съешь ещё этих мягких французских булок, да выпей чаю.</div>
-        <Button className="font-bold">kakashka</Button>
-      </div>
+      <div className="m-8 flex gap-4">Ты труп? Лохич</div>
     </main>
   );
 }
--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,13 +1,14 @@
 import { ModeToggle } from "@/app/shared/ModeToggle/ui/ModeToggle";
 import Link from "next/link";
+import Image from "next/image";
 
-import logo from "../../../../public/header-logo.png";
+import logo from "/src/public/icons/logo.svg";
 
 export const Header = () => {
   return (
     <div className="h-20 sticky flex items-center justify-between py-2 px-8 border-b-[2px]">
       <Link href="/">
-        {/* <Image src={logo} alt="svin logo" className="h-14 w-auto" priority /> */}
+        <Image src={logo} alt="svin logo" className="h-14 w-auto" priority />
       </Link>
       <div className="flex items-center justify-between gap-4">
         <ModeToggle />
