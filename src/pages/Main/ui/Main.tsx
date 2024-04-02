50e72bf820f2ffd0c4f206086e03a58db8d90a88
coca8732 <nub465@gmail.com>
2024-04-02 18:05:51 +0300

    отображение на главной странице

--- src/pages/Main/ui/Main.tsx
+++ src/pages/Main/ui/Main.tsx
@@ -1,17 +1,45 @@
 "use client";
 
-import { useEffect } from "react";
+import { Key, useEffect, useState } from "react";
 
 const Main = () => {
+  const [data, setData] = useState<any>({});
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
   useEffect(() => {
     const searchParams = new URLSearchParams();
 
     searchParams.set("vacancySearchParams", "react");
 
-    fetch(`api/vacancies/getVacanciesAnalysis?${searchParams.toString()}`);
+    const result = fetch(
+      `api/vacancies/getVacanciesAnalysis?${searchParams.toString()}`
+    );
+
+    result.then((res) => res.json()).then((data) => setData(data));
+
+    setIsLoading(false);
   }, []);
 
-  return <div>main</div>;
+  const { analysisContent = {} } = data;
+  const { avgSalary, skills = [], amount = {} } = analysisContent;
+  const { value, valueWithSalary } = amount;
+
+  return isLoading ? (
+    <div>загрузка</div>
+  ) : (
+    <div>
+      <div>{`amount: ${value}, valueWithSalary: ${valueWithSalary}`}</div>
+      <div>{`avgSalary: ${avgSalary}`}</div>
+      {skills.map((skill: any, index: Key) => {
+        const { amount, name, percentage } = skill;
+        return (
+          <div key={index}>
+            {name} - {amount} - {percentage}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Main;
