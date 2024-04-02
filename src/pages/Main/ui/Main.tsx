0d055c20e86ff68ac92fce5f7d26880cd5027f96
coca8732 <91153498+ArtemPasechnyi@users.noreply.github.com>
2024-04-02 17:47:10 +0300

    T-5 Сделал анализ вакансий (up to 2k) (#1)
    Co-authored-by: Cassage <mavrin.2003@list.ru>

--- src/pages/Main/ui/Main.tsx
+++ src/pages/Main/ui/Main.tsx
@@ -1,4 +1,16 @@
+"use client";
+
+import { useEffect } from "react";
+
 const Main = () => {
+  useEffect(() => {
+    const searchParams = new URLSearchParams();
+
+    searchParams.set("vacancySearchParams", "react");
+
+    fetch(`api/vacancies/getVacanciesAnalysis?${searchParams.toString()}`);
+  }, []);
+
   return <div>main</div>;
 };
 
